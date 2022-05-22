5de3c849d3b7d7514927b95043f7d6d0583056e1
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-05-22 23:45:34 +0530

    lll

--- php/register.php
+++ php/register.php
@@ -6,55 +6,59 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         $cPassword = $_POST['cPassword'];
+        $userCheck = '';
 
         session_start();
 
-        $aNo = $_SESSION['accountNo'];
+        $userCheck = $_SESSION['username'];
 
-        if(!empty($username) && !empty($password) && !empty($cPassword)){
-            
-            $userCheck = ($connection -> query("SELECT * FROM useraccount WHERE Username = '$username'"));
 
-            if(mysqli_num_rows($userCheck) > 0){
-                $_SESSION['checkData'] = 'Username is Already taken! Try with another username';
-                header("Location:./errorPage.php");
-            }
-            else{
-                if($password == $cPassword){
-
-                    if(!(strlen($cPassword) >= 6)){
-                        $_SESSION['checkData'] = 'Password is too short! Password must be have at least 6 characters';
-                        header("Location:./errorPage.php");
-
-                    }
-                    else{
-
-                        $sql =($connection->query("UPDATE  useraccount SET Username = '$username', Apassword = '$cPassword' WHERE AccountNo = '$aNo'")); 
-
-                        if($sql){
-                            header("Location:../html/login.html");
-                        }
-                        else{
-                            $_SESSION['checkData'] = 'File Submission is Failed';
-                            header("Location:./errorPage.php");
-
-                        }
-                    }
-                }
-                else{
-                    $_SESSION['checkData'] = 'Passwords are not Matched! Try again';
+            $aNo = $_SESSION['accountNo'];
+    
+            if(!empty($username) && !empty($password) && !empty($cPassword)){
+                
+                $userCheck = ($connection -> query("SELECT * FROM useraccount WHERE Username = '$username'"));
+    
+                if(mysqli_num_rows($userCheck) > 0){
+                    $_SESSION['checkData'] = 'Username is Already taken! Try with another username';
                     header("Location:./errorPage.php");
                 }
-
+                else{
+                    if($password == $cPassword){
+    
+                        if(!(strlen($cPassword) >= 6)){
+                            $_SESSION['checkData'] = 'Password is too short! Password must be have at least 6 characters';
+                            header("Location:./errorPage.php");
+    
+                        }
+                        else{
+    
+                            $sql =($connection->query("UPDATE  useraccount SET Username = '$username', Apassword = '$cPassword' WHERE AccountNo = '$aNo'")); 
+    
+                            if($sql){
+                                header("Location:../html/login.html");
+                            }
+                            else{
+                                $_SESSION['checkData'] = 'File Submission is Failed';
+                                header("Location:./errorPage.php");
+    
+                            }
+                        }
+                    }
+                    else{
+                        $_SESSION['checkData'] = 'Passwords are not Matched! Try again';
+                        header("Location:./errorPage.php");
+                    }
+    
+                }
+            }
+            else{
+                $_SESSION['checkData'] = 'Input fields are empty!';
+                header("Location:./errorPage.php");
+    
             }
         }
-        else{
-            $_SESSION['checkData'] = 'Input fields are empty!';
-            header("Location:./errorPage.php");
 
-        }
-
-    }
     else{
         $errorMessage = 'Something wrong!';
         require_once './errorPage.php';
